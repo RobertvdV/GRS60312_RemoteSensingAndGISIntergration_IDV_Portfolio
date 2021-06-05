a47869f9919bb98ae262c7f1bddbf401416ba458
RobertvdV <robertvandevlasakker@outlook.com>
2021-06-05 10:35:58 +0200

    init

---
+++ GRS60312_RemoteSensingAndGISIntergration_IDV_Portfolio/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Visualization Portfolio for the MGI ACT course',
+    author='Robert van de Vlasakker',
+    license='MIT',
+)
